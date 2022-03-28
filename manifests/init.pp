44d010a803dff71f17b6bda908c872fad7c949ff
treydock <tdockendorf@osc.edu>
2022-03-28 13:32:42 -0400

    Bump default version to 1.17.7 (#6)

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,7 +18,7 @@
 # @param bin_dir
 #   The path to bin directory for go and gofmt symlinks
 class golang (
-  String $version = '1.16.1',
+  String $version = '1.17.7',
   String[1] $os = downcase($facts['kernel']),
   String[1] $arch = $facts['os']['architecture'],
   Stdlib::Absolutepath $download_dir = '/tmp',
